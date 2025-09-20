303c3b8ff91fbf1f679f351774d4a7dc5ab11528
smee <maxim.oransky@gmail.com>
2025-09-20 23:18:49 +0200

    Final linting fixes for v1.5.0

--- Tests/SwiftEmbedTests/PropertyWrapperCacheTests.swift
+++ Tests/SwiftEmbedTests/PropertyWrapperCacheTests.swift
@@ -4,7 +4,7 @@ import Foundation
 
 @Suite("Property Wrapper Cache Tests")
 struct PropertyWrapperCacheTests {
-    
+
     @Test("Property wrappers use shared cache")
     func testPropertyWrappersUseCache() {
         // First instance loads from disk
@@ -12,23 +12,23 @@ struct PropertyWrapperCacheTests {
             @Embedded.JSON(Bundle.module, path: "TestData/sample.json")
             var user: User
         }
-        
+
         // Second instance should get from cache
         struct Instance2 {
             @Embedded.JSON(Bundle.module, path: "TestData/sample.json")
             var user: User
         }
-        
+
         // Direct call also uses same cache
         let directUser = Embedded.getJSON(Bundle.module, path: "TestData/sample.json", as: User.self)
-        
+
         let inst1 = Instance1()
         let inst2 = Instance2()
-        
+
         #expect(inst1.user == inst2.user)
         #expect(inst1.user == directUser)
     }
-    
+
     // Reuse model from other tests
     struct User: Decodable, Equatable {
         let name: String
